44aad3d6a44e63f796f13a1b820f93208c1c3d1a
pkaur515 <kaurprabh2002ks@gmail.com>
2025-02-21 20:06:00 -0500

    Lab-4

---
+++ lab4c.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+dict_york = {'Address': '70 The Pond Rd', 'City': 'Toronto', 'Country': 'Canada', 'Postal Code': 'M3J3M6', 'Province': 'ON'}
+dict_newnham = {'Address': '1750 Finch Ave E', 'City': 'Toronto', 'Country': 'Canada', 'Postal Code': 'M2J2X5', 'Province': 'ON'}
+list_keys = ['Address', 'City', 'Country', 'Postal Code', 'Province']
+list_values = ['70 The Pond Rd', 'Toronto', 'Canada', 'M3J3M6', 'ON']
+
+def create_dictionary(keys, values):
+    return dict(zip(keys, values))  # Combines lists into a dictionary
+
+def shared_values(dict1, dict2):
+    return set(dict1.values()) & set(dict2.values())  # Finds common values in both dictionaries
+
+if __name__ == '__main__':
+    york = create_dictionary(list_keys, list_values)
+    print('York: ', york)
+    common = shared_values(dict_york, dict_newnham)
+    print('Shared Values', common)
